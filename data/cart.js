e1d548c8ce47a52a75d030b2eb6023559de65972
arpitverma00 <arpitverma1907@gmail.com>
2024-09-14 00:47:06 +0530

    Save cart in local Storage

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,24 @@
-export let cart =[{
+export let cart = JSON.parse(localStorage.getItem('cart'));
 
-  productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 2,
+if(!cart){
+  cart = [{
 
-},{
-  productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
+    productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+  
+  },{
+    productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+  
+  }];
+}
 
-}];
+
+
+
+function saveToStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart));
+}
 
 export function addToCart(productId){
 
@@ -29,6 +40,8 @@ export function addToCart(productId){
     });
   }
 
+
+  saveToStorage();
 }
 
 export function removeFromCart(productId){
@@ -44,5 +57,7 @@ export function removeFromCart(productId){
   });
 
   cart = newCart;
+
+  saveToStorage();
 
 }

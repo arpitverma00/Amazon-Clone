6da06ecdbd71b08aff87c801097b88f53a358849
arpitverma00 <arpitverma1907@gmail.com>
2024-09-16 16:17:27 +0530

    Save data and generate HTML for payment summary

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -17,3 +17,17 @@ price : 99
 
 
 }];
+
+
+export function getDeliveryOption(deliveryOptionId){
+  let deliveryOption;
+
+  deliveryOptions.forEach((option) =>{
+    if(option.id === deliveryOptionId){
+      deliveryOption= option;
+    }
+  });
+
+  return deliveryOption || deliveryOptions[0];
+
+}
---
+++ scripts/checkout/paymentSummary.js
@@ -0,0 +1,59 @@
+import {cart} from '../../data/cart.js'; 
+import {getProduct} from '../../data/products.js'; 
+import {getDeliveryOption} from '../../data/deliveryOptions.js';
+
+
+export function renderPaymentSummary(){
+  let productPrice = 0;
+  let shippingPrice = 0; 
+
+  cart.forEach((cartItem) =>{
+    const product = getProduct(cartItem.productId);
+
+    productPrice += product.price * cartItem.quantity;
+
+    const deliveryOption = getDeliveryOption(cartItem.deliveryOptionId);
+    shippingPrice += deliveryOption.price;
+
+
+  });
+
+  //const totalBeforeTax= productPrice + shippingPrice;
+  //const taxPrice=  totalBeforeTax * 0.1;
+  //const total = totalBeforeTax + taxPrice;
+  const total = productPrice + shippingPrice;
+
+  const paymentSummaryHTML = `
+      <div class="payment-summary-title">
+            Order Summary
+          </div>
+
+          <div class="payment-summary-row">
+            <div>Items (3):</div>
+            <div class="payment-summary-money">₹${productPrice.toFixed(2)}</div>
+          </div>
+
+          <div class="payment-summary-row">
+            <div>Shipping &amp; handling:</div>
+            <div class="payment-summary-money">₹${shippingPrice.toFixed(2)}</div>
+          </div>
+
+          <div class="payment-summary-row total-row">
+            <div>Order total:</div>
+            <div class="payment-summary-money">₹${total.toFixed(2)}</div>
+          </div>
+
+          <button class="place-order-button button-primary">
+            Place your order
+          </button>
+
+  
+    `;
+
+
+
+
+  document.querySelector('.js-payment-summary')
+    .innerHTML= paymentSummaryHTML;
+  
+}
